7dae6a1e604927e8eee128b16e8fe68905f2b26e
Tsunemori-H <144467939+Tsunemori-H@users.noreply.github.com>
2023-09-10 16:20:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ class Config(object):
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/4e3b3a92124c048ca4e36.png")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '682111519').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "Sonic_Otakus") 
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "Rename_File") 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001537276104"))
 
     # wes response configuration     
